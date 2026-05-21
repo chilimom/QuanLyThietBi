a2f1ed46268dd3c38848ea529e371d8625951630
Ta Cong Vuong - HPDQ <tacongvuong@hoaphat.com.vn>
2026-05-21 10:20:50 +0700

    Auto-sync: file changes

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const host = process.env.VITE_HOST || "127.0.0.1";
-const port = Number(process.env.VITE_PORT || 3001);
+const host = process.env.VITE_HOST || "10.192.72.45";
+const port = Number(process.env.VITE_PORT || 3000);
  
 export default defineConfig({
   plugins: [react()],
@@ -11,7 +11,7 @@ export default defineConfig({
     port,
     proxy: {
       "/api": {
-        target: process.env.VITE_PROXY_TARGET || "http://localhost:5134",
+        target: process.env.VITE_PROXY_TARGET || "http://10.192.72.45:5173",
         changeOrigin: true,
       },
     },
